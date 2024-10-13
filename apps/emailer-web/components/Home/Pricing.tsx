5180ad18c796bdacc94c5621dae3d607fb6fd0eb
kirtanchandak <themarketingkirtan@gmail.com>
2024-10-13 16:21:18 +0530

    razorpay init & create order

--- apps/emailer-web/components/Home/Pricing.tsx
+++ apps/emailer-web/components/Home/Pricing.tsx
@@ -1,17 +1,60 @@
-import React from "react";
+"use client";
+
+import Script from "next/script";
 import { IconCheck } from "@tabler/icons-react";
+import axios from "axios";
 
 export default function Pricing() {
+  const createOrder = async () => {
+    console.log("fuck");
+
+    const res = await axios.post("/api/payments/createOrder", {
+      amount: 800 * 100,
+    });
+
+    const data = res.data;
+
+    const paymentData = {
+      key: process.env.NEXT_PUBLIC_RAZORPAY_KEY_ID,
+      order_id: data.id,
+
+      handler: async function (response: any) {
+        // verify payment
+        const res = await fetch("/api/payments/verifyOrder", {
+          method: "POST",
+          body: JSON.stringify({
+            orderId: response.razorpay_order_id,
+            razorpayPaymentId: response.razorpay_payment_id,
+            razorpaySignature: response.razorpay_signature,
+          }),
+        });
+        const data = await res.json();
+        console.log(data);
+        if (data.isOk) {
+          // do whatever page transition you want here as payment was successful
+          alert("Payment successful");
+        } else {
+          alert("Payment failed");
+        }
+      },
+    };
+    const payment = new (window as any).Razorpay(paymentData);
+    payment.open();
+  };
   return (
     <>
-      <section id="pricing" className="py-20 px-6">
+      <section id="pricing" className="pb-20 px-6">
+        <Script
+          type="text/javascript"
+          src="https://checkout.razorpay.com/v1/checkout.js"
+        />
         <div className="max-w-5xl mx-auto space-y-12">
           <h2 className="text-3xl font-bold text-center">Pricing</h2>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-            <div className="flex flex-col justify-center items-center bg-white p-8 rounded-lg shadow">
+            <div className="flex flex-col justify-center items-center bg-white md:p-8 p-6 rounded-lg shadow">
               <div className="flex items-center gap-3">
-                <h3 className="text-2xl font-bold">Pay As You Go</h3>
-                <p className="bg-green-100 text-green-500 p-2 rounded-md font-bold text-2xl">
+                <h3 className="md:text-2xl text-lg font-bold">Pay As You Go</h3>
+                <p className="bg-green-100 text-green-500 p-2 rounded-md font-bold md:text-2xl text-lg text-nowrap">
                   Flexible Plan
                 </p>
               </div>
@@ -42,14 +85,19 @@ export default function Pricing() {
                   Pay for what you use
                 </p>
               </div>
-              <button className="w-full text-xl font-bold py-2 mt-6 rounded-md bg-[#27C08D] hover:bg-[#27C08D]/90 text-white">
+              <button
+                onClick={createOrder}
+                className="w-full text-xl font-bold py-2 mt-6 rounded-md bg-[#27C08D] hover:bg-[#27C08D]/90 text-white"
+              >
                 Get Started
               </button>
             </div>
-            <div className="flex flex-col justify-center items-center bg-white p-8 rounded-lg shadow">
+            <div className="flex flex-col justify-center items-center bg-white md:p-8 p-6 rounded-lg shadow">
               <div className="flex items-center gap-3">
-                <h3 className="text-2xl font-bold">Unlimited Access</h3>
-                <p className="bg-blue-100 text-blue-500 p-2 rounded-md font-bold text-2xl">
+                <h3 className="md:text-2xl text-md font-bold">
+                  Unlimited Access
+                </h3>
+                <p className="bg-blue-100 text-blue-500 p-2 rounded-md font-bold md:text-2xl text-lg text-nowrap">
                   Best Value
                 </p>
               </div>
